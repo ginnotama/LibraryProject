abbf6a77ca55eb6884723f6c50d2b373380aa998
ZGY <2878689220@qq.com>
2024-09-27 11:16:24 +0800

    评论表,借阅表增加状态字段. 新增评论, 借阅相关查询

--- sql/book/comment.sql
+++ sql/book/comment.sql
@@ -7,8 +7,19 @@ show CREATE TABLE comment
 desc comment
 
 -- 分页查询某本书的所有评论, 按照评论时间排序
-SELECT * FROM `comment` WHERE book_id = '' ORDER BY comment_create_time desc LIMIT (0.10)
+SELECT * FROM `comment` WHERE book_id = '' ORDER BY comment_create_time desc LIMIT 0.10;
 
+-- 分页查询查询某一用户所有评论
+SELECT * FROM `comment` WHERE user_id = '' ORDER BY comment_create_time desc LIMIT 0.10;
 
--- 分页查询查询某一用户所有评论, 按照评论时间排序
-SELECT * FROM `comment` WHERE user_id = '' ORDER BY comment_create_time desc LIMIT (0.10)
+-- 插入评论
+INSERT INTO `comment` VALUES(NULL, 'comment_desc',
+'comment_create_time',
+'book_id',
+'user_id')
+
+-- 更新评论
+UPDATE `comment` SET comment_desc = '' WHERE comment_id ='';
+
+-- 更新评论状态(已删除, 不可见, 正常)
+UPDATE `comment` SET comment_state = '' WHERE comment_id ='';
