0ee68b5f65cbbc149f2c0b29391242f5b0cec777
JiamaoWang <wangjiamao2@gmail.com>
2024-10-02 19:01:36 +0800

    高级搜索

---
+++ book-manager/src/mock/hobbyMock.js
@@ -0,0 +1,9 @@
+const hobby = ['Philosophy', 'Religion', 'SocialSciences', 'Agricultural Science', 'Technology', 'Transportation', 'Aviation and aerospace',
+  'Politics', 'Law', 'Military', 'Astronomy', 'Earth Sciences', 'Biological Sciences', 'Medicine',
+  'Language and literature', 'Art', 'History', 'Geography', 'Natural sciences', 'Mathematical Science', 'Chemistry',
+  'Environmental science', 'Safety science', 'Comprehensive books'
+];
+
+export {
+  hobby
+};
